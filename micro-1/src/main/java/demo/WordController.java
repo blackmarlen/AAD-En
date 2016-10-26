ab4d0d06457622071d391e997cc125fb4c1c2b05
blackmarlen <blackmarlen@yahoo.com>
2016-10-26 13:24:24 +0300

    Update WordController.java

--- micro-1/src/main/java/demo/WordController.java
+++ micro-1/src/main/java/demo/WordController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@EnableConfigurationProperties
 @ConfigurationProperties(prefix="cuvinte")
 public class WordController {
 
